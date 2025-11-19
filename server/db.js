a2a9df383b2fb67810812d5cdf5d8efb0ae7164e
gatsby6060 <uniline123@naver.com>
2025-11-19 23:18:08 +0900

    456

--- server/db.js
+++ server/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'test1234',
+    // password: 'test1234',
+    password: 'ezen',
     database: 'mysqldb' // db 이름
 });
 
